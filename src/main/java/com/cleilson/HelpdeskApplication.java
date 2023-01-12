82e19098be2d6b546ed5a43e1b4c3d0a52770a93
cleilsonjose <cleilsonrock@gmail.com>
2023-01-12 14:16:27 -0300

    first commit

---
+++ src/main/java/com/cleilson/HelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.cleilson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
