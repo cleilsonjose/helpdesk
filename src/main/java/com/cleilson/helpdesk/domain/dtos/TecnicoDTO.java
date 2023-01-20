b3aa502841340556e58cec9c9bb3f9af8b5684c8
cleilsonjose <cleilsonrock@gmail.com>
2023-01-20 14:35:22 -0300

    Validacao criacao de novo usuario

--- src/main/java/com/cleilson/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/cleilson/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.cleilson.helpdesk.domain.Tecnico;
 import com.cleilson.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -14,9 +16,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	@NotNull(message = "O campo NOME é obrigatório")
 	protected String nome;
+	@NotNull(message = "O campo CPF é obrigatório")
 	protected String cpf;
+	@NotNull(message = "O campo EMAIL é obrigatório")
 	protected String email;
+	@NotNull(message = "O campo SENHA é obrigatório")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 
