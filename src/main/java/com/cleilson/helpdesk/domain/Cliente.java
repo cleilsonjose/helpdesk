f60fa8612223b36dc05db9b024d7744c88d793f3
cleilsonjose <cleilsonrock@gmail.com>
2023-01-23 10:14:38 -0300

    CRUD cliente

--- src/main/java/com/cleilson/helpdesk/domain/Cliente.java
+++ src/main/java/com/cleilson/helpdesk/domain/Cliente.java
@@ -2,10 +2,12 @@ package com.cleilson.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.cleilson.helpdesk.domain.dtos.ClienteDTO;
 import com.cleilson.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -26,6 +28,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
---
+++ src/main/java/com/cleilson/helpdesk/domain/dtos/ClienteDTO.java
@@ -0,0 +1,107 @@
+package com.cleilson.helpdesk.domain.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotNull;
+
+import com.cleilson.helpdesk.domain.Cliente;
+import com.cleilson.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	protected Integer id;
+	@NotNull(message = "O campo NOME é obrigatório")
+	protected String nome;
+	@NotNull(message = "O campo CPF é obrigatório")
+	protected String cpf;
+	@NotNull(message = "O campo EMAIL é obrigatório")
+	protected String email;
+	@NotNull(message = "O campo SENHA é obrigatório")
+	protected String senha;
+	protected Set<Integer> perfis = new HashSet<>();
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	protected LocalDate dataCriacao = LocalDate.now();
+	
+	public ClienteDTO() {
+		super();
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfis(Perfil perfil) {
+		this.perfis.add(perfil.getCodigo());
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	
+
+}
