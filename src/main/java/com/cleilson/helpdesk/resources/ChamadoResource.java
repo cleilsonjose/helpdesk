4e813b6efac0adc94a5a6b9e86640cc7f1ebf668
cleilsonjose <cleilsonrock@gmail.com>
2023-01-23 11:56:34 -0300

    findById Chamado

---
+++ src/main/java/com/cleilson/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,26 @@
+package com.cleilson.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cleilson.helpdesk.domain.Chamado;
+import com.cleilson.helpdesk.domain.dtos.ChamadoDTO;
+import com.cleilson.helpdesk.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+	@Autowired
+	private ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+}
