ade74b52d832e7d3e2623fca1aa70b08386c4a7e
cleilsonjose <cleilsonrock@gmail.com>
2023-01-23 15:41:43 -0300

    CREATE Chamado

--- src/main/java/com/cleilson/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/cleilson/helpdesk/domain/dtos/ChamadoDTO.java
@@ -2,6 +2,8 @@ package com.cleilson.helpdesk.domain.dtos;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.cleilson.helpdesk.domain.Chamado;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -13,11 +15,17 @@ public class ChamadoDTO {
 	private LocalDate dataAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	@NotNull(message = "O campo PRIORIDADE é requerido")
 	private Integer prioridade;
+	@NotNull(message = "O campo STATUS é requerido")
 	private Integer status;
+	@NotNull(message = "O campo TITULO é requerido")
 	private String titulo;
+	@NotNull(message = "O campo OBSERVAÇÕES é requerido")
 	private String observacoes;
+	@NotNull(message = "O campo TÉCNICO é requerido")
 	private Integer tecnico;
+	@NotNull(message = "O campo CLIENTE é requerido")
 	private Integer cliente;
 	private String nomeTecnico;
 	private String nomeCliente;
